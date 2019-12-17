71142934612d1d5bc0d979e09c37f6916d41b2a8
aaa <aaa@aaa.com>
2019-12-17 12:24:50 +0200

    191217c probeserver implemented

--- README.md
+++ README.md
@@ -2,42 +2,43 @@
 Class tinymce skeleton/template
 
 # Changes:
--
+- 191217c added probeserver for custom file load
 -
 -
 -
 
 
-
-
+# Examples:
+- http://192.168.1.200/tinymce_class/tinymce.html?probeserver&file=lesson05
+- http://192.168.1.200/tinymce_class/tinymce.html?probeserver&file=giorti25mart01
 
 # Template for numbered table (for lesson with songs etc)
 ```html
 <table>
 <tr><td>
 1.
-</td></td>
+</td><td>
 
 </td></tr><!-- new row-->
 <tr><td>
 2.
-</td></td>
+</td><td>
 
 </td></tr><!-- new row-->
 <tr><td>
 3.
-</td></td>
+</td><td>
 
 </td></tr><!-- new row-->
 <tr><td>
 4.
-</td></td>
+</td><td>
 
 </td></tr><!-- new row-->
 <tr><td>
 5.
-</td></td>
-
+</td><td>
+ 
 </td></tr>
 </table>
 ```
---
+++ tinymce_lesson_js_footer.js
@@ -0,0 +1,76 @@
+//<script type="text/javascript">
+/*
+* tinymce_lesson_js_footer  (put this in same folder as parent html)
+*
+*
+* Changes:
+* ver191216 -initial tinymce version - sample http://192.168.1.200/tinymce_class/tinymce.html?probeserver&file=lesson05
+* ver190411 -noopengame (initial ver) (removes opengame -in case we run a plain Http server with no PHP)
+* ver190410 -opengame (initial ver)
+* ver190404 -showdiv
+* ver190402 -all scripts are now in footer
+* 
+* example : index.html?showdiv5\&timer3\&probeserver , 
+*/
+
+    var first_click=true;
+    var url_time_param=location.search.substring(1).indexOf("time");
+
+var url_string = location;// "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
+var url = new URL(location.href);
+
+var file_name_param = url.searchParams.get("file");
+
+console.log(file_name_param);
+
+
+//+++++++++++++++++++ probeserver4lessons v02 1911+++++++++++++++++
+var server_probing_enabled=false;
+var url_probeserver=location.search.substring(1).indexOf("probeserver");
+if (url_probeserver!==-1)server_probing_enabled=true;
+
+var timer_server_probe = 30000; //probe every 30 seconds
+var server_probe_file="lessons/lesson05.htm";
+
+if (file_name_param!==-1) {
+    server_probe_file='lessons/'+file_name_param +".htm";
+    console.log(server_probe_file);
+    
+
+    }
+
+var jsonrequestInterval = function () {
+    console.log("The request was send");
+    // <hr><div id="probeserver"></div><hr> 
+    var jsonrequestIntervaled = new XMLHttpRequest();
+    var random_number=Math.random(); // OLD was=Date.prototype.getTime;
+    jsonrequestIntervaled.open("GET", server_probe_file+"?"+random_number, true); // Date.prototype.getTime is used to avoid caching
+    jsonrequestIntervaled.send();
+    jsonrequestIntervaled.onreadystatechange = function () {
+        if (jsonrequestIntervaled.readyState == 4) {
+            console.log("The request was made and returned results (with random number="+random_number);
+            var response_string =jsonrequestIntervaled.responseText;
+
+            //always add our extra text
+            document.getElementById("probeserver").innerHTML = response_string;
+            
+            //in case we put the word reload, refresh browser
+            if (response_string.indexOf("reload") !== -1) {
+                console.log("refreshing browser");
+                //document.getElementById("probeserver").innerHTML = response_string;
+                window.location.reload(true);
+            }
+            
+        }
+    };
+    
+};
+
+if(server_probing_enabled) setInterval(jsonrequestInterval, timer_server_probe);
+
+//-------------------probeserver ---------------
+
+
+
+
+//</script>
