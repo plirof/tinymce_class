d84431c0813d2e3f7b0cbeab8892226cea3c4d5a
aaa <aaa@aaa.com>
2020-04-06 00:58:54 +0300

    200405c - seem to work test01

--- serverside/index.php
+++ serverside/index.php
@@ -2,4 +2,18 @@
 //print_r($_POST); 
 print_r($_REQUEST); 
 
+$values = '';
+foreach($_REQUEST as $key=>$value) {
+    $values.= $key . "=" . $value . "\t";
+}
+
+$filename = 'answers.htm';
+$firstline='<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><BR>\n';
+if (!file_exists($filename)) {   $values=$firstline.$values; }
+file_put_contents($filename, $values."<HR>\n", FILE_APPEND);
+//$handle = fopen($filename, 'w');
+//fwrite($handle, $values);
+//fclose($handle);
+
+
 ?>
